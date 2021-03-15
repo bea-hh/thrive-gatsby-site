490525768bfe996bc2543e75016cc2f2dde6b63e
Beatriz Herce-Hagiwara <beahh@Beas-Air.home>
2021-03-14 20:18:14 -0400

    First commit, TODO: customize ReadMe

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,12 @@ module.exports = {
   siteMetadata: {
     title: "Thrive Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: "src/images/beahh-logo.png",
+      },
+    },
+  ],
 };
